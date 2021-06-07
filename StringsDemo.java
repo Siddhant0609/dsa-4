adf03903570e22bfd3e02ddf9bc623aa96693da3
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-06-07 11:54:36 +0530

    Add files via upload

---
+++ StringsDemo.java
@@ -0,0 +1,11 @@
+
+public class StringsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Ravi";
+		String name2 = "Ravi";
+		System.out.println(name == name2);
+	}
+
+}
