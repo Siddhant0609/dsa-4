9bd9c846eba1c36fd856abf020d3cefe189e71a6
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-06-16 11:59:59 +0530

    Add files via upload

--- DSAlgoArray/src/PairSumTwoPointer.java
+++ DSAlgoArray/src/PairSumTwoPointer.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
 
 public class PairSumTwoPointer {
 
 	static void pairSum(int arr[]) {
-		int k = 9;
+//		int k = 9;
+		int k = 11;
+		Arrays.sort(arr);
 		int i = 0;
 		int j = arr.length - 1;
 		while(i < j) {
